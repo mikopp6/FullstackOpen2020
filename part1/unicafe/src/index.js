c0086ca8f1e93d790d59cbd1cf1df5d1e61ffafd
Miko <miko.palojarvi@student.oulu.fi>
2020-06-26 12:56:02 +0300

    starting part2 unicafe

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
