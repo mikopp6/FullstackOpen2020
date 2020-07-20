cb05b7caa5a9267436dc356fad457ba447e722d4
Miko <miko.palojarvi@student.oulu.fi>
2020-07-20 16:53:20 +0300

    4.15 and 4.16 done

---
+++ part4/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+mongoose.set('useFindAndModify', false)
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 3
+  },
+  passwordHash: String,
+  name: String,
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+userSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('User', userSchema)
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -3,110 +3,197 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+
+const User = require('../models/user')
 const Blog = require('../models/blog')
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
-  }
+
+describe('blog tests', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    for (let blog of helper.initialBlogs) {
+      let blogObject = new Blog(blog)
+      await blogObject.save()
+    }
+  })
+  
+  test('blogs json return', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+  
+  test('unique identifier is named id', async () => {
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd[0].id).toBeDefined()
+  })
+  
+  test('adding valid blog', async () => {
+    const newBlog = {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const title = blogsAtEnd.map(r => r.title)
+    expect(title).toContain('Canonical string reduction')
+  
+  })
+  
+  test('blog with likes missing defaults it to 0', async () => {
+    const newBlog = {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      __v: 0
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    const addedBlog = blogsAtEnd[2]
+    expect(addedBlog.likes).toEqual(0)
+  
+  })
+  
+  test('blog with title and url missing fails', async () => {
+    const newBlog = {
+      _id: '5a422b3a1b54a676234d17f9',
+      author: 'Edsger W. Dijkstra',
+      likes: 12,
+      __v: 0
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+  
+  test('deleting blog', async () => {
+    await api
+      .delete('/api/blogs/5a422aa71b54a676234d17f8')
+      .expect(204)
+  })
+  
+  test('updating blog', async () => {
+    const updatedBlog = {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 6,
+      __v: 0
+    }
+  
+    await api
+      .put('/api/blogs/5a422aa71b54a676234d17f8')
+      .send(updatedBlog)
+      .expect(204)
+  })
 })
 
-test('blogs json return', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+describe('user tests, one initial user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const user = new User({ username: 'root', name: 'Sys Admin', password: 'hunter1' })
+    await user.save()
+  })
+
+  test('users json return', async () => {
+    await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('creating new user succeeds', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'miko123',
+      name: 'Miko Palojärvi',
+      password: 'correcthorsebatterystaple'
+    }
+    
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(user => user.username)
+    expect(usernames).toContain(newUser.username)
+  })
+  
+  test('non-unique username fails', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'admin123',
+      password: 'pw1234'
+    }
+    
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length)  
+  })
+
+  test('missing pw fails', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: '123test',
+      name: 'admin123'
+    }
+    
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('password missing or too short')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length)  
+  })
+
 })
 
-test('unique identifier is named id', async () => {
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd[0].id).toBeDefined()
-})
 
-test('adding valid blog', async () => {
-  const newBlog = {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const title = blogsAtEnd.map(r => r.title)
-  expect(title).toContain('Canonical string reduction')
-
-})
-
-test('blog with likes missing defaults it to 0', async () => {
-  const newBlog = {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    __v: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  const addedBlog = blogsAtEnd[2]
-  expect(addedBlog.likes).toEqual(0)
-
-})
-
-test('blog with title and url missing fails', async () => {
-  const newBlog = {
-    _id: '5a422b3a1b54a676234d17f9',
-    author: 'Edsger W. Dijkstra',
-    likes: 12,
-    __v: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-})
-
-test('deleting blog', async () => {
-  await api
-    .delete('/api/blogs/5a422aa71b54a676234d17f8')
-    .expect(204)
-})
-
-test('updating blog', async () => {
-  const updatedBlog = {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 6,
-    __v: 0
-  }
-
-  await api
-    .put('/api/blogs/5a422aa71b54a676234d17f8')
-    .send(updatedBlog)
-    .expect(204)
-})
 
 afterAll(() => {
   mongoose.connection.close()
