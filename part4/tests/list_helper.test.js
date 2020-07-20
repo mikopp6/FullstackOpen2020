2804c18073a6201a7518719b7cd499b0f5447d66
Miko <miko.palojarvi@student.oulu.fi>
2020-07-20 12:28:24 +0300

    parts 4.6 and 4.7 done (without lodash!)

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -112,3 +112,31 @@ describe('favorite blog', () => {
   
 
 })
+
+describe('author with most blogs', () => {
+  test('when list has only one blog', () => {
+    expect(listHelper.mostBlogs(listWithOneBlog)).toEqual({author: 'Edsger W. Dijkstra', blogs: 1})
+  })
+
+  test('of empty list is zero', () => {
+    expect(listHelper.mostBlogs(emptyList)).toBe(null)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    expect(listHelper.mostBlogs(biggerList)).toEqual({author: 'Robert C. Martin', blogs: 3})
+  })
+})
+
+describe('author with most likes across all blogs', () => {
+  test('when list has only one blog', () => {
+    expect(listHelper.mostLikes(listWithOneBlog)).toEqual({author: 'Edsger W. Dijkstra', likes: 5})
+  })
+
+  test('of empty list is zero', () => {
+    expect(listHelper.mostLikes(emptyList)).toBe(null)
+  })
+  
+  test('of a bigger list is calculated right', () => {
+    expect(listHelper.mostLikes(biggerList)).toEqual({author: 'Edsger W. Dijkstra', likes: 17})
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,4 @@
+
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
@@ -21,13 +22,42 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((max, blog) => (max && max.likes > blog.likes) ? max : blog, null)
 }
 
-//const mostBlogs = (blogs) => {
-//  return {author: '', blogs: }
-//}
+const mostBlogs = (blogs) => {
+  let tally = []
+  const authorsBlogs = blogs.map(blog => {return {author:blog.author, blogs:1}})
+
+  authorsBlogs.forEach(element => {
+    const index = tally.findIndex(e => e.author === element.author)
+    if (index === -1) {
+      tally.push({author:element.author, blogs:element.blogs})
+    } else {
+      tally[index].blogs += element.blogs
+    }
+  })
+
+  return tally.reduce((max, elem) => (max && max.blogs > elem.blogs) ? max : elem, null) 
+}
+
+const mostLikes = (blogs) => {
+  let tally = []
+  const authorLikes = blogs.map(blog => {return {author:blog.author, likes:blog.likes}})
+  
+  authorLikes.forEach(element => {
+    const index = tally.findIndex(e => e.author === element.author)
+    if (index === -1) {
+      tally.push({author:element.author, likes:element.likes})
+    } else {
+      tally[index].likes += element.likes
+    }
+  })
+
+  return tally.reduce((max, elem) => (max && max.likes > elem.likes) ? max : elem, null) 
+}
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  //mostBlogs
+  mostBlogs,
+  mostLikes
 }
