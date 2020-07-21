85a8456ba5ed61ecf61d60d743bc5fccc00763a2
Miko <miko.palojarvi@student.oulu.fi>
2020-07-21 10:20:43 +0300

    token-auth should work (tests broke tho)

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,6 +2,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.
     find({}).populate('user')
@@ -11,8 +21,13 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token ||!decodedToken.id) {
+    return response.status(401).json({ error: 'token missin or invalid'})
+  }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -25,6 +40,7 @@ blogsRouter.post('/', async (request, response) => {
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
+  
   response.json(savedBlog)
 
 })
