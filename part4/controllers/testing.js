e85d1f78c604029c09496ada212dfd11c4a532b5
Miko <miko.palojarvi@student.oulu.fi>
2020-07-27 18:56:53 +0300

    removed debugging

--- part4/controllers/testing.js
+++ part4/controllers/testing.js
@@ -2,10 +2,7 @@ const router = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-console.log('in testing')
-
 router.post('/reset', async (request, response) => {
-  console.log('in testing router post')
   await Blog.deleteMany({})
   await User.deleteMany({})
 
