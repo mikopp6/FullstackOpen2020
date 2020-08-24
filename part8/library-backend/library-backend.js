4d4a22a0010093333e76a2a60d326bfc9e71649d
Miko <miko.palojarvi@student.oulu.fi>
2020-08-24 06:55:36 +0300

    8.1 and 8.2 done

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -84,12 +84,34 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author:  String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
   Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: () => books,
+    allAuthors: () => authors
   }
 }
 
