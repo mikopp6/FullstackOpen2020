2803d9ea6c10256566ee86d41a036f2a24475974
Miko <miko.palojarvi@student.oulu.fi>
2020-08-26 12:07:45 +0300

    8.6 done

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -105,6 +106,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -128,6 +138,17 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter(book => book.author === root.name).length
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = {...args, id: uuid()}
+      books = books.concat(book)
+      if (!authors.includes({name: args.author})) {
+        const author = {name: args.author, id: uuid()}
+        authors = authors.concat(author)
+      }
+      return book
     }
   }
 }
