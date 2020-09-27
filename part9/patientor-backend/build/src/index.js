429db320f343f8e8defd9d561e6d46e7ebf0176d
Miko <miko.palojarvi@student.oulu.fi>
2020-09-27 21:16:45 +0300

    9.10 and 9.11 done (added cors-thingy)

--- part9/patientor-backend/build/src/index.js
+++ part9/patientor-backend/build/src/index.js
@@ -4,10 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const patients_1 = __importDefault(require("./routes/patients"));
 const diagnoses_1 = __importDefault(require("./routes/diagnoses"));
 const app = express_1.default();
 app.use(express_1.default.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors_1.default());
 const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import patientRouter from './routes/patients';
 import diagnoseRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
 const PORT = 3001;
 
